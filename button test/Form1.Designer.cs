283e45adfeae1133c97af7ea769f7686a388f285
chiangjouyi <chiangjouyi@gmail.com>
2018-10-16 18:43:08 +0800

    notes

---
+++ button test/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace button_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn清除選號 = new System.Windows.Forms.Button();
+            this.btn當期對獎 = new System.Windows.Forms.Button();
+            this.btn獎號自動生成 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.BackColor = System.Drawing.Color.Transparent;
+            this.label.Enabled = false;
+            this.label.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label.ForeColor = System.Drawing.Color.Black;
+            this.label.Location = new System.Drawing.Point(26, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(684, 52);
+            this.label.TabIndex = 2;
+            this.label.Click += new System.EventHandler(this.label_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(80, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "本期台彩開出獎號為";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Ivory;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Location = new System.Drawing.Point(3, 414);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(880, 290);
+            this.label2.TabIndex = 5;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btn清除選號
+            // 
+            this.btn清除選號.BackColor = System.Drawing.Color.Gold;
+            this.btn清除選號.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn清除選號.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btn清除選號.Location = new System.Drawing.Point(623, 620);
+            this.btn清除選號.Name = "btn清除選號";
+            this.btn清除選號.Size = new System.Drawing.Size(240, 54);
+            this.btn清除選號.TabIndex = 8;
+            this.btn清除選號.Text = "清  除  選  號";
+            this.btn清除選號.UseVisualStyleBackColor = false;
+            this.btn清除選號.Click += new System.EventHandler(this.btn清除選號_Click_1);
+            // 
+            // btn當期對獎
+            // 
+            this.btn當期對獎.BackColor = System.Drawing.Color.DarkRed;
+            this.btn當期對獎.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn當期對獎.ForeColor = System.Drawing.Color.White;
+            this.btn當期對獎.Location = new System.Drawing.Point(623, 409);
+            this.btn當期對獎.Name = "btn當期對獎";
+            this.btn當期對獎.Size = new System.Drawing.Size(240, 54);
+            this.btn當期對獎.TabIndex = 7;
+            this.btn當期對獎.Text = "當  期  對  獎";
+            this.btn當期對獎.UseVisualStyleBackColor = false;
+            this.btn當期對獎.Click += new System.EventHandler(this.btn當期對獎_Click_1);
+            // 
+            // btn獎號自動生成
+            // 
+            this.btn獎號自動生成.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btn獎號自動生成.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn獎號自動生成.ForeColor = System.Drawing.Color.White;
+            this.btn獎號自動生成.Location = new System.Drawing.Point(623, 517);
+            this.btn獎號自動生成.Name = "btn獎號自動生成";
+            this.btn獎號自動生成.Size = new System.Drawing.Size(240, 54);
+            this.btn獎號自動生成.TabIndex = 6;
+            this.btn獎號自動生成.Text = "獎號自動生成";
+            this.btn獎號自動生成.UseVisualStyleBackColor = false;
+            this.btn獎號自動生成.Click += new System.EventHandler(this.btn獎號自動生成_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Ivory;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.Color.Firebrick;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label3.Location = new System.Drawing.Point(80, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(783, 51);
+            this.label3.TabIndex = 9;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.Color.Navy;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Location = new System.Drawing.Point(32, 745);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 153);
+            this.label4.TabIndex = 10;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.ForeColor = System.Drawing.Color.DarkRed;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label5.Location = new System.Drawing.Point(238, 745);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 35);
+            this.label5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.ForeColor = System.Drawing.Color.Navy;
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label6.Location = new System.Drawing.Point(291, 780);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 35);
+            this.label6.TabIndex = 12;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::button_test.Properties.Resources.圖片24;
+            this.pictureBox1.Location = new System.Drawing.Point(-35, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(909, 187);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(623, 726);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 54);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "自選號請直接點選號碼球";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightYellow;
+            this.ClientSize = new System.Drawing.Size(863, 969);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn清除選號);
+            this.Controls.Add(this.btn當期對獎);
+            this.Controls.Add(this.btn獎號自動生成);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn清除選號;
+        private System.Windows.Forms.Button btn當期對獎;
+        private System.Windows.Forms.Button btn獎號自動生成;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ button test/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button_test
+{
+    public partial class Form1 : Form
+    {
+        //類別變數
+        List<Button> myButtons = new List<Button>();
+        string 當期台彩號碼 = "   04   06   10   11   12   13   14   15   19   20   21   22      ";
+        string[,] arrayNumber = new string[,] { { "01", "02", "03", "04", "05", "06" }, { "07", "08", "09", "10", "11", "12" }, { "13", "14", "15", "16", "17", "18" }, { "19", "20", "21", "22", "23", "24" } };
+        int click = 0;
+        string str自選號碼 = "";
+        int[] selectnum = new int[] { };
+        ToolTip tt1 = new ToolTip();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            產生按鈕(4, 6);
+          
+            label2.SendToBack();
+            label1.BringToFront();
+            label3.BringToFront();
+            timer1.Enabled = true;
+            timer1.Interval = 500;
+            tt1.AutoPopDelay = 20000;
+            tt1.ToolTipTitle = "*****基本玩法 & 獎金分配方式*****";
+            tt1.SetToolTip(pictureBox1, "\n雙贏彩是一種樂透型遊戲，您必須從01~24的號碼中任選12個號碼進行投注。\n開獎時，開獎單位將隨機開出12個號碼，這一組號碼就是該期雙贏彩的中獎號碼，也稱為「獎號」。\n您的12個選號如果全部對中當期開出之12個號碼，或者全部未對中，均為中頭獎。\n\n\n各獎項的中獎方式如下表：\n\n     獎項                                  中獎方式\n\n     頭獎            對中當期12個獎號 或 對中當期0個獎號\n     貳獎            對中當期任11個獎號 或 對中任1個獎號\n     參獎            對中當期10個獎號 或 對中當期任2個獎號\n     肆獎            對中當期9個獎號 或 對中當期任3個獎號\n\n\n雙贏彩所有獎項皆為固定獎項，各獎項金額如下：\n\n       獎項                   頭獎                   貳獎            參獎        肆獎\n\n     單注獎金     $15,000,000       $100,000       $500       $100");   
+        }
+
+        void 產生按鈕(int row, int column)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    Button EachButton = new Button();
+                    System.Drawing.Drawing2D.GraphicsPath aCircle = new System.Drawing.Drawing2D.GraphicsPath();
+                    EachButton.BringToFront();
+                    EachButton.Size = new Size(80, 80);
+                    aCircle.AddEllipse(new Rectangle(0, 0, 75, 75));
+                    EachButton.Region = new Region(aCircle);
+                    EachButton.Location = new Point(50 + 90 * j, 380 + 85 * i);
+                    EachButton.Text = arrayNumber[i, j].ToString();
+                    EachButton.Name = "btn" + arrayNumber[i, j].ToString();
+                    EachButton.Font = new Font("微軟正黑體", 22, FontStyle.Bold);
+                    EachButton.FlatStyle = FlatStyle.Standard;
+                    EachButton.BackColor = Color.Gold;
+                    EachButton.ForeColor = Color.MidnightBlue;
+                    EachButton.Click += new EventHandler(EachButton_Click);
+
+                    Controls.Add(EachButton);
+                    myButtons.Add(EachButton);
+                }
+            }
+        }
+
+        void EachButton_Click(object sender, EventArgs e)
+        {
+            Button mybtn = (Button)sender;
+            click++;
+            if (click <= 12)
+            {
+                mybtn.ForeColor = Color.White;
+                mybtn.BackColor = Color.Green;
+                mybtn.Enabled = false;
+                str自選號碼 += mybtn.Text.ToString() + ",";
+                label.Text = str自選號碼.Substring(0, str自選號碼.Length - 1);  //label在UI中隱藏                    
+            }
+            else
+            {
+                DialogResult drResult; //MessageBox的回傳值
+                drResult = MessageBox.Show("您選取的獎號為\n" + label.Text.ToString() + "\n\n是否保留選號?", "********您已選取12個號碼********", MessageBoxButtons.YesNo);
+                if (drResult == DialogResult.No)
+                {
+                    mybtn.Enabled = true;
+                    清除所有選號();
+                }
+            }
+        }
+
+        void 清除所有選號()
+        {
+            click = 0;
+            str自選號碼 = "";
+            label.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+            int x = 0;
+            Button allbtn = myButtons[x];
+            for (x = 0; x < 24; x++)
+            {
+                myButtons[23].Enabled = true;
+                allbtn.Enabled = true;
+                allbtn = myButtons[x];
+                allbtn.BackColor = Color.Gold;
+                allbtn.ForeColor = Color.MidnightBlue;
+            }
+        }
+
+        private void btn獎號自動生成_Click_1(object sender, EventArgs e)
+        {
+            清除所有選號();
+            Random Rnd = new Random();
+            Button[] RndBtn = new Button[24];
+            string str單項 = "";
+            string str總項 = "";
+            for (int i = 0; i < 12; i++)
+            {
+                RndBtn[i] = myButtons[Rnd.Next(24)];
+
+                for (int j = 0; j < i; j++)
+                {
+                    while (RndBtn[j] == RndBtn[i])
+                    {
+                        j = 0;
+                        RndBtn[i] = myButtons[Rnd.Next(24)];
+                    }
+                }
+                RndBtn[i].BackColor = Color.Navy;
+                RndBtn[i].ForeColor = Color.Snow;
+                str單項 += RndBtn[i].Text.ToString() + ",";
+            }
+            str總項 = str單項;
+            label.Text = str總項.Substring(0, str總項.Length - 1);
+            DialogResult drResult; //MessageBox的回傳值
+            drResult = MessageBox.Show("電腦自動選號為\n" + label.Text.ToString() + "\n\n是否保留選號?", "********已隨機選取12個號碼********", MessageBoxButtons.YesNo);
+            if (drResult == DialogResult.No)
+            {
+                清除所有選號();
+            }
+        }
+
+        private void btn清除選號_Click_1(object sender, EventArgs e)
+        {
+            清除所有選號();
+        }
+
+        private void btn當期對獎_Click_1(object sender, EventArgs e)
+        {
+            string[] selectnum = label.Text.Split(',');
+            string[] lotterynum = new string[] { "04", "06", "10", "11", "12", "13", "14", "15", "19", "20", "21", "22" };
+            Array.Sort(selectnum);
+            Array.Sort(lotterynum);
+            string str相符獎號 = "";
+            int bingo = 0;
+            Button[] BingoBtn = new Button[24];
+            int[] bingonum = new int[]{};
+
+            if ((selectnum.Length == 12) && (lotterynum.Length == 12))
+            {
+                bool hasCommonElements = selectnum.Intersect(lotterynum).Count() > 0;
+                string[] commonElements = selectnum.Intersect(lotterynum).ToArray();
+                for (int i = 0; i < commonElements.Length; i++)
+                {
+                    str相符獎號 += commonElements[i]+"   ";
+                    bingo = commonElements.Count();
+                    //把commonElements[i]轉成數字陣列
+                    bingonum = Array.ConvertAll(commonElements, int.Parse);
+                    BingoBtn[i] = myButtons[bingonum[i]];
+                    BingoBtn[i].BackColor = Color.DarkRed;
+                    BingoBtn[i].ForeColor = Color.Snow;
+                }
+               
+                if (bingo == 12 || bingo == 11 || bingo == 10 || bingo == 9 || bingo == 3 || bingo == 2 || bingo == 1 || bingo == 0)
+                {
+                    label4.Text = "恭喜您中了\n\n共有\n相符獎號為";
+                    label6.Text = "\n個獎號相符";
+                    if (bingo == 12 || bingo == 0)
+                    { label5.Text = "頭獎!!!   單注獎金為$15,000,000 \n\n" + bingo + "\n" + str相符獎號; }
+                    else if (bingo == 11 || bingo == 1)
+                    { label5.Text = "貳獎!!    單注獎金為$100,000 \n\n" + bingo + "\n" + str相符獎號; }
+                    else if (bingo == 10 || bingo == 2)
+                    {label5.Text = "參獎!     單注獎金為$500\n\n" + bingo + "\n" + str相符獎號; }
+                    else {label5.Text = "肆獎      單注獎金為$100\n\n" + bingo + "\n" + str相符獎號; }
+                }
+                else
+                {
+                    label4.Text = "很遺憾您\n\n共有\n相符獎號為";
+                    label5.Text = "沒有中獎                      \n\n" + bingo + "\n" + str相符獎號;
+                    label6.Text = "\n個獎號相符";
+                }
+            } else
+            {
+                MessageBox.Show("請點選12個號碼","",MessageBoxButtons.OK);
+            }
+      
+        }
+
+
+
+
+
+
+        private void label_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string temp = 當期台彩號碼.Substring(0, 1);
+            當期台彩號碼 = 當期台彩號碼.Substring(1, 當期台彩號碼.Length - 1) + temp;
+            label3.Text = 當期台彩號碼;
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+}
+
